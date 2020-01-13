fa9cb109d8f61f88df3f5e972e4ef678947f085c
JasonJohn <794733453@qq.com>
2020-01-13 16:15:16 +0800

    fix: lineCharts修改

--- src/components/charts/lineCharts.vue
+++ src/components/charts/lineCharts.vue
@@ -50,6 +50,15 @@ export default {
       }
 
       this.defaultOptions = {
+        title:{
+          left: 'center',
+          top: fontSize(0.2),
+          textStyle: {
+            color: '#fff',
+            fontSize:fontSize(0.18),
+            fontWeight: 500
+          }
+        },
         tooltip: {
           trigger: 'axis',
           axisPointer: { // 坐标轴指示器，坐标轴触发有效
